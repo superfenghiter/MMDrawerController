b44f20dcccc3c1ef551965985346b9d65e2f0d8f
superfeng <wangxiaofengmail@126.com>
2021-01-04 15:19:40 +0800

    add target

---
+++ KitchenSink/MMDrawerController/MMDrawerController.h
@@ -0,0 +1,19 @@
+//
+//  MMDrawerController.h
+//  MMDrawerController
+//
+//  Created by superfeng on 2021/1/4.
+//  Copyright © 2021 Mutual Mobile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MMDrawerController.
+FOUNDATION_EXPORT double MMDrawerControllerVersionNumber;
+
+//! Project version string for MMDrawerController.
+FOUNDATION_EXPORT const unsigned char MMDrawerControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMDrawerController/PublicHeader.h>
+
+
